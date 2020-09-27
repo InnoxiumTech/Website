db8889c63518042d58b00028254ed4a5dbd7f5c4
ShadowChild <shadychild12@gmail.com>
2020-09-27 19:26:13 +0100

    Update markdown pane to add syntax highlighting

--- src/component/MarkdownPane.js
+++ src/component/MarkdownPane.js
@@ -1,12 +1,29 @@
 import styles from "./Markdown.module.sass";
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import '../../node_modules/highlight.js/styles/atom-one-dark-reasonable.css'
+var hljs = require("highlight.js");
+var md = require("markdown-it")({
+    // langPrefix: "",
+
+    highlight: function (str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+            try {
+                return '<pre class="hljs"><code>' +
+                    hljs.highlight(lang, str, true).value +
+                    '</code></pre>';
+            } catch (__) {}
+        }
+
+        return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+    }
+});
 
 export default class MarkdownPane extends React.Component {
 
     state = {
 
-        content: null,
+        content: "",
     }
 
     componentDidMount() {
@@ -19,11 +36,13 @@ export default class MarkdownPane extends React.Component {
 
     render() {
 
-        const { content } = this.state;
+        const content = this.state.content;
+
+        console.log(content);
 
         return (
-            <div className={styles.markdown}>
-                <ReactMarkdown source={content}/>
+            <div className={styles.markdown} dangerouslySetInnerHTML={{__html: md.render(content)}}>
+                {/*<ReactMarkdown source={content}/>*/}
             </div>
         )
     }
